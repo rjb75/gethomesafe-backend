0a94f09e5f5c035f11f0052999c0db6b27e01bd0
Nicholas Knapton <nickknapton12@gmail.com>
2024-02-18 15:17:41 -0700

    feat: User Info Enpoint

--- userService/src/app.ts
+++ userService/src/app.ts
@@ -6,6 +6,8 @@ import {isUserHome} from "./controllers/isUserHome";
 import {signupValidationSchema} from "./validators/signupValidation";
 import {isUserHomeValidatorSchema} from "./validators/isUserHomeValidation";
 import {updateAccountValidationSchema} from "./validators/updateAccountValidator";
+import {getUserInfoValidationSchema} from "./validators/getUserInfoValidation";
+import {getUserInfo} from "./controllers/getUserInfo";
 
 const app = express();
 const port = 3000;
@@ -15,6 +17,7 @@ app.use(json())
 app.post('/api/signup', signupValidationSchema, signup);
 app.put('/api/updateAccount', updateAccountValidationSchema, updateAccount);
 app.get('/api/isUserHome', isUserHomeValidatorSchema, isUserHome);
+app.get('/api/getUserInfo', getUserInfoValidationSchema, getUserInfo);
 
 app.listen(port, () => {
     console.log(`User service listening on port ${port}`);
