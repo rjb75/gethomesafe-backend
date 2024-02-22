54046bcf3f0478bdf9abf8098dced9168af9ed01
Robert Brown <brown.rjb75@gmail.com>
2024-02-22 16:50:05 -0700

    feat: implement firebase auth in user service

---
+++ userService/src/firebase/firebaseConfig.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Firebase config
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MSG_SDR_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+export const firebaseAuth = getAuth(initializeApp(firebaseConfig));
