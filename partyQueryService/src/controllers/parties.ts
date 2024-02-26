b2c2b6517b2199a50a9d0e4106426478038438d7
Anand Patel <anandpp2001@gmail.com>
2024-02-25 23:47:15 -0700

    feat: query endpoints

--- partyQueryService/src/controllers/parties.ts
+++ partyQueryService/src/controllers/parties.ts
@@ -1,5 +1,18 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import { getParties } from "../services/queries";
 
-export const parties = (req: Request, res: Response) => {
-    res.send("Hello parties");
-}
+export const parties = async (req: Request, res: Response) => {
+  const valResult = validationResult(req);
+  if (!valResult.isEmpty()) {
+    return res.status(300).send(valResult.array());
+  }
+
+  try {
+    const partyData = await getParties();
+
+    res.status(200).json(partyData);
+  } catch (e) {
+    res.status(500).send("Server Error");
+  }
+};
--- partyQueryService/src/controllers/partyStatus.ts
+++ partyQueryService/src/controllers/partyStatus.ts
@@ -1,5 +1,28 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import { getPartyStatus } from "../services/queries";
 
-export const partyStatus = async (req: Request, res: Response) => {
-    res.send("Hello party status");
+export interface partyStatusRequestQuery {
+  partyId: string;
 }
+
+export const partyStatus = async (
+  req: Request<{}, {}, {}, partyStatusRequestQuery>,
+  res: Response
+) => {
+  const valResult = validationResult(req);
+  if (!valResult.isEmpty()) {
+    return res.status(300).send(valResult.array());
+  }
+  const partyId = req.query.partyId;
+
+  try {
+    const partyData = await getPartyStatus(partyId);
+    if (!partyData) {
+      return res.status(404).send("Invalid invite code - Party not found");
+    }
+    res.status(200).json(partyData);
+  } catch (e) {
+    res.status(500).send("Server Error");
+  }
+};
