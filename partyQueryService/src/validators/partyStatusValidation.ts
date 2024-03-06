1e5b26a2b82c78b91b3ecf22f12fbcb491f6c114
Anand Patel <73033746+anandp1@users.noreply.github.com>
2024-03-06 11:06:11 -0700

    Feat: party query endpoints (#9)
    * feat: query endpoints
    
    * fix:rebase fix

--- partyQueryService/src/validators/partyStatusValidation.ts
+++ partyQueryService/src/validators/partyStatusValidation.ts
@@ -1,3 +1,12 @@
-import {checkSchema} from "express-validator";
+import { checkSchema } from "express-validator";
 
-export const partyStatusValidationSchema = checkSchema({})
+export const partyStatusValidationSchema = checkSchema({
+  partyId: {
+    in: ["query"],
+    isString: true,
+    isLength: {
+      options: { min: 1, max: 255 },
+    },
+    errorMessage: "partyId must be a string with length between 1 and 255",
+  },
+});
